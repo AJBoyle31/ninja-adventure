9914d1fd207811f52592943e7e11486b7b3792c1
Andrew <ajboyle31@gmail.com>
2023-09-24 13:57:18 -0400

    weapon testing

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -5,6 +5,7 @@ const SPEED: int = 100
 
 var velocity: Vector2 = Vector2.ZERO
 var player_state
+var current_weapon
 
 enum State { IDLE, WALK, ATTACK }
 
@@ -12,10 +13,12 @@ enum State { IDLE, WALK, ATTACK }
 onready var player_animation_player: AnimationPlayer = $PlayerAnimationPlayer
 onready var player_animation_tree: AnimationTree = $PlayerAnimationTree
 onready var player_animation_state = player_animation_tree.get("parameters/playback")
+onready var player_weapons = $WeaponPosition2D/Weapon
 
 func _ready() -> void:
 	player_animation_tree.active = true
 	player_state = State.IDLE
+	current_weapon = player_weapons.get_child(0)
 
 func _physics_process(_delta) -> void:
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
